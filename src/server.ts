4fcbf0d6376e982dd06fc78bcf4a264af75c7f05
Edson Batista <henrique.ti1196@gmail.com>
2020-06-27 14:57:32 -0300

    chore(typeorm): configure entities patha and install typeorm dep

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,12 @@
+/**
+ * `reflact-matadata` dever ser importado no primeiro arquivo da aplicação,
+ * ele é uma dependencia do typeorm para dar suporte no uso de decorator com ts.
+ *
+ * Na própria doc do typeorm é solicitado a instalação:
+ * @see https://typeorm.io/#/undefined/installation
+ */
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
 
