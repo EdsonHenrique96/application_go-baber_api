4ee7b150bf271735307836b7a523855ef554e853
Edson Batista <henrique.ti1196@gmail.com>
2020-06-27 14:59:52 -0300

    refactor(route): now use typeorm

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmetService from '../services/CreateAppointmentServices';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (_request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (_request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   const { provider, date } = request.body;
 
   const formatedDate = parseISO(date);
 
   try {
-    const CreateAppointmet = new CreateAppointmetService(
-      appointmentsRepository,
-    );
+    const CreateAppointmet = new CreateAppointmetService();
 
-    const createdAppointment = CreateAppointmet.execute({
+    const createdAppointment = await CreateAppointmet.execute({
       date: formatedDate,
       provider,
     });
